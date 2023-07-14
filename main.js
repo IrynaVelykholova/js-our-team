d34e7c0acb8947b6cebb27c7b1b370abaed15191
IrynaVelykholova <irynavely93@gmail.com>
2023-07-14 15:31:55 +0200

    creato array con le informazioni

---
+++ main.js
@@ -0,0 +1,38 @@
+const membriDelTeam = [
+    {
+        nome: "Wayne",
+        cognome: "Barnett",
+        ruolo: "Founder & CEO",
+        fotoProfilo: "204-2043250_pepe-emote-png-download-pepe-png-transparent-png.png",
+    },
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        ruolo: "Chief Editor",
+        fotoProfilo: "225-2256254_pepe-the-frog-with-sunglasses-png-transparent-png.png",
+    },
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        ruolo: "Office Manage",
+        fotoProfilo: "11585265315cgqlabbxpg9rd44wtgih6bj5lmj9k2kklkj8ivsldlrq3q3n4vxjqfhsm91q9pwxykdtgbm8owuayvitx5mqbsay0se8p35vscxk.png",
+    },
+    {
+        nome: "Angela",
+        cognome: "Lopez",
+        ruolo: "Social Media Manager",
+        fotoProfilo: "pepehype-emote-11549535085phfevfijot.png",
+    },
+    {
+        nome: "Scott",
+        cognome: "Estrada",
+        ruolo: "Developer",
+        fotoProfilo: "png-transparent-pepe-the-frog-batman-internet-meme-pepe-frog-comics-mammal-cat-like-mammal.png",
+    },
+    {
+        nome: "Barbara",
+        cognome: "Ramos",
+        ruolo: "Graphic Designer",
+        profilePic: "st,small,507x507-pad,600x600,f8f8f8.jpg",
+    },
+];
